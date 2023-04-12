2e04e6d7c1ae4b49f3c5161fc44685d53fc718f9
Mutindi Hope <msndindi@outlook.com>
2023-04-12 06:52:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Replace 'YOUR-NAME' with the actual name you want to display
+newHeader.textContent = 'Hope is the champion';
+
+// Append the newHeader to the DOM
+document.body.appendChild(newHeader);
+
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
